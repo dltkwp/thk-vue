8e60e224d7f8b4dbfcfde8c31dcbee514b42c01b
thk9 <119013888@qq.com>
2018-07-20 17:27:23 +0800

    temp

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">首页信息地址页面 name:{{name}}</div>
-    <div class="dashboard-text">roles:<span v-for='role in roles' :key='role'>{{role}}</span></div>
-  </div>
+  <el-main>
+      <div class="dashboard-container">
+        <div class="dashboard-text">首页信息地址页面 name:{{name}}</div>
+        <div class="dashboard-text">roles:<span v-for='role in roles' :key='role'>{{role}}</span></div>
+      </div>
+  </el-main>
 </template>
 
 <script>
---
+++ src/views/product/productList.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-main>
+
+    </el-main>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
